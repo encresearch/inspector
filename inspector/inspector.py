d3676d35bdb922dcc3fa7bb21f9ca7fb6cef9d7d
gstrenge <gstrenge01@gmail.com>
2019-06-11 11:18:58 -0400

    Refactored Code to be put into functions and be more organized. Similar to how it is organized in Publisher

--- inspector/inspector.py
+++ inspector/inspector.py
@@ -4,6 +4,65 @@ import time
 import math
 import json
 
+HOST = "iot.eclipse.org"
+PORT = 1883
+KEEPALIVE = 60
+topic = "data/anomaly"
+client_id = "/Inspector"
+
+
+
+def createJSON(topic, location, time_init, time_duration):
+    return json.dumps({
+            'topic': topic,
+            'location': location,
+            'time_init': time_init,
+            'time_duration': time_duration
+           })
+
+def connect_to_broker(client_id, host, port, keepalive, on_connect, on_publish):
+    # Params -> Client(client_id=””, clean_session=True, userdata=None, protocol=MQTTv311, transport=”tcp”)
+    # We set clean_session False, so in case connection is lost, it'll reconnect with same ID
+    client = mqtt.Client(client_id=client_id, clean_session=False)
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    connection = client.connect(host, port, keepalive)
+    return (client, connection)
+
+
+def main():
+
+    def on_connect(client, userdata, flags, rc):
+        if rc==0:
+            print(f"Connected OK: {client}")
+        else:
+            print(f"Bad Connection Returned (Code: {rc})")
+        pass
+
+    def on_publish(client, userdata, result):
+        # Function for clients's specific callback when pubslishing message
+        print("Published")
+        pass
+
+    client, connection = connect_to_broker(client_id=client_id, host=HOST, port=PORT, keepalive=KEEPALIVE, on_connect=on_connect, on_publish=on_publish)
+
+    client.loop_start()
+
+    dataExaminer = examineData()
+
+    while True:
+
+        packageToSend = next(dataExaminer)
+        if packageToSend != None:
+            print(packageToSend)
+            client.publish(topic, packageToSend)
+
+    client.loop_stop()
+
+    client.disconnect()
+
+
+
 def examineData():
 
     initialTime = time.time()
@@ -11,7 +70,6 @@ def examineData():
     thresholdBroken = False
     detectedTime = 0
     endTime = 0
-
 
     while True:
 
@@ -30,43 +88,14 @@ def examineData():
             if thresholdBroken == True:
                 thresholdBroken = False
                 endTime = time.time()
+                inspectorPackageJSON = createJSON('gas_sensor', 'How to do location?', detectedTime-initialTime, endTime-detectedTime)
 
-                inspectorPackageDict = {
-                                        'topic': 'gas_sensor',
-                                        'location': 'NA-EAST',
-                                        'time_init': detectedTime-initialTime,
-                                        'time_duration': endTime-detectedTime
-                                        }
-                yield inspectorPackageDict
-                #initialTime = time.time()
+                yield inspectorPackageJSON
         yield None
 
 
 
-broker = "iot.eclipse.org"
-port = 1883
-keepalive = 60
 
-today= str(datetime.datetime.now().strftime("%m-%d-%y %H:%M"))
 
-inspectorPackageDict = {
-                    'topic': 'gas_sensor',
-                    'location': 'NA-EAST',
-                    'time_init': today,
-                    'time_duration': 12
-                    }
-
-inspectorPackageJSON = json.dumps(inspectorPackageDict)
-
-topic = "data/anomaly"
-client = mqtt.Client("notiferTestENC")
-client.connect(host=broker, keepalive=keepalive, port=port)
-
-client.loop_start()
-dataExaminer = examineData()
-while True:
-
-    packageToSend = next(dataExaminer)
-    if packageToSend != None:
-        print(packageToSend)
-        #client.publish(topic, inspectorPackageJSON)
+if __name__ == '__main__':
+    main()
