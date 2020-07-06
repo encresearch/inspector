f99e0bf507272cb4cfb182c89c5f2fee05d1168c
Sebastian Arboleda <seb.arboleda@outlook.com>
2020-07-06 19:24:59 -0400

    Add simulated threshold functions and docker files. (#1)

--- inspector/data_examiner.py
+++ inspector/data_examiner.py
@@ -1,22 +1,19 @@
-import datetime
 import time
 import json
-#time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))
+
 
 class DataExaminer():
 
     dataExaminers = []
     dataExaminersAccess = {}
 
-    def __init__(self, sensorName, location,ThresholdValue, mode=0):
+    def __init__(self, sensorName, location, ThresholdValue, mode=2):
         self.location = location
         self.sensorName = sensorName
 
-        #print("Measurements thing init")
         self.measurementName = location + "_" + sensorName
         DataExaminer.dataExaminers.append(self.measurementName)
         DataExaminer.dataExaminersAccess[self.measurementName] = self
-        #print("Measureend")
 
         self.thresholdValue = 0
         self.lowerThreshold = 0
@@ -31,13 +28,9 @@ class DataExaminer():
 
         self.anomalyFunction = lambda a: a+10
 
-        #print("Modes")
         if mode == 0:
-            #print("beginModes")
             self.thresholdValue = ThresholdValue[0]
-            #print("MidModes")
             self.anomalyFunction = self.greaterThan
-            #print("EndModes")
         elif mode == 1:
             self.thresholdValue = ThresholdValue[0]
             self.anomalyFunction = self.lessThan
@@ -53,10 +46,10 @@ class DataExaminer():
             self.higherThreshold = ThresholdValue[1]
             self.anomalyFunction = self.outOfRange
         else:
-            if len(thresholdValue) == 1:
+            if len(ThresholdValue) == 1:
                 self.thresholdValue = ThresholdValue[0]
                 self.anomalyFunction = self.greaterThan
-            elif len(thresholdValue) == 2:
+            elif len(ThresholdValue) == 2:
                 self.lowerThreshold = ThresholdValue[0]
                 self.higherThreshold = ThresholdValue[1]
                 self.anomalyFunction = self.outOfRange
@@ -64,15 +57,18 @@ class DataExaminer():
                 self.thresholdValue = 0
                 self.anomalyFunction = self.greaterThan
 
-
     def greaterThan(self, value):
         return value > self.thresholdValue
+
     def lessThan(self, value):
         return value < self.thresholdValue
+
     def equal(self, value):
         return value == self.thresholdValue
+
     def inRange(self, value):
         return value > self.lowerThreshold and value < self.higherThreshold
+
     def outOfRange(self, value):
         return value < self.lowerThreshold or value > self.higherThreshold
 
@@ -90,19 +86,27 @@ class DataExaminer():
             if self.anomalyFunction(value):
                 if d:
                     print("Anomaly Function True")
-                if self.thresholdBroken == False:
+                if self.thresholdBroken is False:
                     if d:
                         print(f"Anomaly Detected {self.measurementName}")
                     self.thresholdBroken = True
                     self.influxTimeStamp_start = timestamp
-                    #2019-07-18T11:33:02.155326976Z
-                    #self.anomalyDetectedTime = DateTime.strftime(string,"%Y-%m-%DT%H:%M:$S.")
                     self.anomalyDetectedTime = time.time()
 
-                    anomalies.append(self.createJSON(topic=[self.location, self.sensorName, self.measurementName], anomaly_status="Started", location=self.location, time_init=self.influxTimeStamp_start, time_duration="N/A"))
+                    anomalies.append(self.createJSON(
+                        topic=[
+                            self.location,
+                            self.sensorName,
+                            self.measurementName
+                        ],
+                        anomaly_status="Started",
+                        location=self.location,
+                        time_init=self.influxTimeStamp_start,
+                        time_duration="N/A"
+                    ))
 
             else:
-                if self.thresholdBroken == True:
+                if self.thresholdBroken is True:
                     if d:
                         print("Loss of threshold")
                     self.thresholdBroken = False
@@ -110,19 +114,37 @@ class DataExaminer():
                         print(f"Anomaly Ended {self.measurementName}")
                     self.influxTimeStamp_end = timestamp
                     self.anomalyEndTime = time.time()
-                    anomalies.append(self.createJSON(topic=[self.location, self.sensorName, self.measurementName], anomaly_status="Ended", location=self.location, time_init=self.influxTimeStamp_start, time_duration=self.anomalyEndTime-self.anomalyDetectedTime))
-
+                    anomalies.append(self.createJSON(
+                        topic=[
+                            self.location,
+                            self.sensorName,
+                            self.measurementName
+                        ],
+                        anomaly_status="Ended",
+                        location=self.location,
+                        time_init=self.influxTimeStamp_start,
+                        time_duration=(
+                            self.anomalyEndTime - self.anomalyDetectedTime
+                        )
+                    ))
 
         return anomalies
 
-
-    def createJSON(self, topic, anomaly_status, location, time_init, time_duration):
-        ''' Helper function to quickly create a JSON String
-        with the following format'''
-        return json.dumps({
+    def createJSON(
+        self,
+        topic,
+        anomaly_status,
+        location,
+        time_init,
+        time_duration
+    ):
+        """Helper function to quickly create a JSON String."""
+        return json.dumps(
+            {
                 'topic': topic,
                 'anomaly_status': anomaly_status,
                 'location': location,
                 'time_init': time_init,
                 'time_duration': time_duration
-               })
+            }
+        )
